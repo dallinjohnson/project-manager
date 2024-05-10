690b197a27c3eb29ef58c14d648a41efadc94c66
Dallin Johnson <13dallin13@gmail.com>
2024-05-09 21:13:02 -0600

    Create DTO classes for Task, Project, and User domain models

---
+++ src/main/java/com/dallinjohnson/projectmanager/dto/ProjectDTO.java
@@ -0,0 +1,62 @@
+package com.dallinjohnson.projectmanager.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ProjectDTO {
+
+    private Long id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private boolean isComplete;
+    private List<TaskDTO> tasks;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    public List<TaskDTO> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskDTO> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ src/main/java/com/dallinjohnson/projectmanager/dto/TaskDTO.java
@@ -0,0 +1,64 @@
+package com.dallinjohnson.projectmanager.dto;
+
+import com.dallinjohnson.projectmanager.domain.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TaskDTO {
+
+    private Long id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private boolean isComplete;
+    private List<User> assignedUsers;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    public List<User> getAssignedUsers() {
+        return assignedUsers;
+    }
+
+    public void setAssignedUsers(List<User> assignedUsers) {
+        this.assignedUsers = assignedUsers;
+    }
+}
